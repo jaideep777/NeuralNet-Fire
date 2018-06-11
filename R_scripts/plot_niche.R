c60e2d9b9b8239b926287efe03a98c8bdd467c5e
jaideep777 <jaideep777@gmail.com>
2018-06-11 15:24:58 +0530

    removed tabs from plotting scripts on master for easy comparison with dccc

--- R_scripts/plot_niche.R
+++ R_scripts/plot_niche.R
@@ -3,91 +3,91 @@
 #### PREDICTED FIRES - CALIBRATION ####
 
 for (model_name in c("gfed_xcf")){
-  for (iter in 9:9){
-    
-    model = paste0(model_name, "_", iter)
-    
-    # Simulation name ("" or "india" or "ssaplus" etc)
-    sim_name           <- paste0("ssaplus", "/", model)
-    
-    # Directories to the fire model folder
-    fire_dir           <- "/home/jaideep/codes/FIRE_CODES" # root directory for fire codes
-    
-    #### Init ####
-    suffix = ""
-    if (sim_name != "") suffix = paste0(suffix,"_",sim_name)
-    output_dir = paste0("output",suffix)
-    
-    dataset = "eval"
-    system("mkdir -p figures")
+for (iter in 9:9){
 
-    # datf_train = read.fireData(dataset = "train", dir=paste0(fire_dir, "/fire_aggregateData/",output_dir))
-    # datf_eval = read.fireData(dataset = "eval", dir=paste0(fire_dir, "/fire_aggregateData/",output_dir))
-    # datf_test = read.fireData(dataset = "test", dir=paste0(fire_dir, "/fire_aggregateData/",output_dir))
-    
-    # datf = rbind(datf_eval, datf_test, datf_train)
-    
-    datf = read.fireData_gfed(dataset = dataset, dir=paste0(fire_dir, "/fire_aggregateData/",output_dir))
-    
-   
-    plot.niche = function(datf, name="", max.baclass=11){
-      # png(filename = paste0("niche(",model,"_",name,").png"), width = 400*3, height = 500*3, res = 300)
-      png(filename = paste0("niche(",model,"_",name,").png"), width = 512*3, height = 790*3, res = 300)
-      
-      par(mfrow = c(4,2), mar=c(4,5,1,1), oma=c(1,1,1,1), cex.axis=1.5, cex.lab=1.5)
-      
-      col.obs = rgb(datf$baclass/max.baclass, 1-datf$baclass/max.baclass, 0)
-      col.pred = rgb(datf$baclass_pred/max.baclass, 1-datf$baclass_pred/max.baclass, 0)
-      
-      col.obs = colorRampPalette(colors = c("mediumspringgreen","red"))(100)[floor(datf$baclass/max.baclass*100)+1]
-      col.pred = colorRampPalette(colors = c("mediumspringgreen","red"))(100)[floor(datf$baclass_pred/max.baclass*100)+1]
-      
-      plot(datf$lmois~datf$dxl, pch=20, cex=0.1, col=col.obs, xlab = "Fuel mass", ylab="Fuel Moisture")
-      points(datf$lmois~datf$dxl, pch=20, cex=datf$baclass/4, col=col.obs)
-      plot(datf$lmois~datf$dxl, pch=20, cex=0.1, col=col.pred, xlab = "Fuel mass", ylab="Fuel Moisture")
-      points(datf$lmois~datf$dxl, pch=20, cex=datf$baclass_pred/4, col=col.pred)
-      
-      # plot(datf$rh~datf$ts, pch=20, cex=0.1, col=col.obs, xlim=c(270-273.16,320-273.16), xlab = "Temperature", ylab="Rel Humidity")
-      # points(datf$rh~datf$ts, pch=20, cex=datf$baclass/4, col=col.obs)
-      # plot(datf$rh~datf$ts, pch=20, cex=0.1, col=col.pred, xlim=c(270-273.16,320-273.16), xlab = "Temperature", ylab="Rel Humidity")
-      # points(datf$rh~datf$ts, pch=20, cex=datf$baclass_pred/4, col=col.pred)
-      
-      plot(datf$rh~datf$ts, pch=20, cex=0.1, col=col.obs, xlim=c(250,320), xlab = "Temperature", ylab="Rel Humidity")
-      points(datf$rh~datf$ts, pch=20, cex=datf$baclass/4, col=col.obs)
-      plot(datf$rh~datf$ts, pch=20, cex=0.1, col=col.pred, xlim=c(250,320), xlab = "Temperature", ylab="Rel Humidity")
-      points(datf$rh~datf$ts, pch=20, cex=datf$baclass_pred/4, col=col.pred)
-      
-      
-      plot(datf$ts~datf$wsp, pch=20, cex=0.1, col=col.obs, xlab = "Wind speed", ylab="Temperature")
-      points(datf$ts~datf$wsp, pch=20, cex=datf$baclass/4, col=col.obs)
-      plot(datf$ts~datf$wsp, pch=20, cex=0.1, col=col.pred, xlab = "Wind speed", ylab="Temperature")
-      points(datf$ts~datf$wsp, pch=20, cex=datf$baclass_pred/4, col=col.pred)
+model = paste0(model_name, "_", iter)
 
-      plot(datf$agri_frac~datf$logpop, pch=20, cex=0.1, col=col.obs, xlab = "Pop density", ylab="Cropland Frac")
-      points(datf$agri_frac~datf$logpop, pch=20, cex=datf$baclass/4, col=col.obs)
-      plot(datf$agri_frac~datf$logpop, pch=20, cex=0.1, col=col.pred, xlab = "Pop density", ylab="Cropland Frac")
-      points(datf$agri_frac~datf$logpop, pch=20, cex=datf$baclass_pred/4, col=col.pred)
+# Simulation name ("" or "india" or "ssaplus" etc)
+sim_name           <- paste0("ssaplus", "/", model)
 
-      # plot(datf$agri_frac~datf$forest_frac, pch=20, cex=0.1, col=col.obs, xlab = "Pop dens", ylab="Agri Frac")
-      # points(datf$agri_frac~datf$forest_frac, pch=20, cex=datf$baclass/4, col=col.obs)
-      # plot(datf$agri_frac~datf$forest_frac, pch=20, cex=0.1, col=col.pred, xlab = "Pop dens", ylab="Agri Frac")
-      # points(datf$agri_frac~datf$forest_frac, pch=20, cex=datf$baclass_pred/3, col=col.pred)
+# Directories to the fire model folder
+fire_dir           <- "/home/jaideep/codes/FIRE_CODES" # root directory for fire codes
 
-      # plot(datf$forest_frac~datf$logpop, pch=20, cex=0.1, col=col.obs, xlab = "Pop dens", ylab="Forest Frac")
-      # points(datf$forest_frac~datf$logpop, pch=20, cex=datf$baclass/4, col=col.obs)
-      # plot(datf$forest_frac~datf$logpop, pch=20, cex=0.1, col=col.pred, xlab = "Pop dens", ylab="Forest Frac")
-      # points(datf$forest_frac~datf$logpop, pch=20, cex=datf$baclass_pred/3, col=col.pred)
-      # 
-      dev.off()
-    }
+#### Init ####
+suffix = ""
+if (sim_name != "") suffix = paste0(suffix,"_",sim_name)
+output_dir = paste0("output",suffix)
 
-    setwd(paste0(fire_dir,"/fire_aggregateData/output",suffix,"/figures" ))
-    
-    plot.niche(datf, "ALL")  # MIXED
-    
+dataset = "eval"
+system("mkdir -p figures")
 
-    
-  }
+# datf_train = read.fireData(dataset = "train", dir=paste0(fire_dir, "/fire_aggregateData/",output_dir))
+# datf_eval = read.fireData(dataset = "eval", dir=paste0(fire_dir, "/fire_aggregateData/",output_dir))
+# datf_test = read.fireData(dataset = "test", dir=paste0(fire_dir, "/fire_aggregateData/",output_dir))
+
+# datf = rbind(datf_eval, datf_test, datf_train)
+
+datf = read.fireData_gfed(dataset = dataset, dir=paste0(fire_dir, "/fire_aggregateData/",output_dir))
+
+
+plot.niche = function(datf, name="", max.baclass=11){
+  # png(filename = paste0("niche(",model,"_",name,").png"), width = 400*3, height = 500*3, res = 300)
+  png(filename = paste0("niche(",model,"_",name,").png"), width = 512*3, height = 790*3, res = 300)
+  
+  par(mfrow = c(4,2), mar=c(4,5,1,1), oma=c(1,1,1,1), cex.axis=1.5, cex.lab=1.5)
+  
+  col.obs = rgb(datf$baclass/max.baclass, 1-datf$baclass/max.baclass, 0)
+  col.pred = rgb(datf$baclass_pred/max.baclass, 1-datf$baclass_pred/max.baclass, 0)
+  
+  col.obs = colorRampPalette(colors = c("mediumspringgreen","red"))(100)[floor(datf$baclass/max.baclass*100)+1]
+  col.pred = colorRampPalette(colors = c("mediumspringgreen","red"))(100)[floor(datf$baclass_pred/max.baclass*100)+1]
+  
+  plot(datf$lmois~datf$dxl, pch=20, cex=0.1, col=col.obs, xlab = "Fuel mass", ylab="Fuel Moisture")
+  points(datf$lmois~datf$dxl, pch=20, cex=datf$baclass/4, col=col.obs)
+  plot(datf$lmois~datf$dxl, pch=20, cex=0.1, col=col.pred, xlab = "Fuel mass", ylab="Fuel Moisture")
+  points(datf$lmois~datf$dxl, pch=20, cex=datf$baclass_pred/4, col=col.pred)
+  
+  # plot(datf$rh~datf$ts, pch=20, cex=0.1, col=col.obs, xlim=c(270-273.16,320-273.16), xlab = "Temperature", ylab="Rel Humidity")
+  # points(datf$rh~datf$ts, pch=20, cex=datf$baclass/4, col=col.obs)
+  # plot(datf$rh~datf$ts, pch=20, cex=0.1, col=col.pred, xlim=c(270-273.16,320-273.16), xlab = "Temperature", ylab="Rel Humidity")
+  # points(datf$rh~datf$ts, pch=20, cex=datf$baclass_pred/4, col=col.pred)
+  
+  plot(datf$rh~datf$ts, pch=20, cex=0.1, col=col.obs, xlim=c(250,320), xlab = "Temperature", ylab="Rel Humidity")
+  points(datf$rh~datf$ts, pch=20, cex=datf$baclass/4, col=col.obs)
+  plot(datf$rh~datf$ts, pch=20, cex=0.1, col=col.pred, xlim=c(250,320), xlab = "Temperature", ylab="Rel Humidity")
+  points(datf$rh~datf$ts, pch=20, cex=datf$baclass_pred/4, col=col.pred)
+  
+  
+  plot(datf$ts~datf$wsp, pch=20, cex=0.1, col=col.obs, xlab = "Wind speed", ylab="Temperature")
+  points(datf$ts~datf$wsp, pch=20, cex=datf$baclass/4, col=col.obs)
+  plot(datf$ts~datf$wsp, pch=20, cex=0.1, col=col.pred, xlab = "Wind speed", ylab="Temperature")
+  points(datf$ts~datf$wsp, pch=20, cex=datf$baclass_pred/4, col=col.pred)
+
+  plot(datf$agri_frac~datf$logpop, pch=20, cex=0.1, col=col.obs, xlab = "Pop density", ylab="Cropland Frac")
+  points(datf$agri_frac~datf$logpop, pch=20, cex=datf$baclass/4, col=col.obs)
+  plot(datf$agri_frac~datf$logpop, pch=20, cex=0.1, col=col.pred, xlab = "Pop density", ylab="Cropland Frac")
+  points(datf$agri_frac~datf$logpop, pch=20, cex=datf$baclass_pred/4, col=col.pred)
+
+  # plot(datf$agri_frac~datf$forest_frac, pch=20, cex=0.1, col=col.obs, xlab = "Pop dens", ylab="Agri Frac")
+  # points(datf$agri_frac~datf$forest_frac, pch=20, cex=datf$baclass/4, col=col.obs)
+  # plot(datf$agri_frac~datf$forest_frac, pch=20, cex=0.1, col=col.pred, xlab = "Pop dens", ylab="Agri Frac")
+  # points(datf$agri_frac~datf$forest_frac, pch=20, cex=datf$baclass_pred/3, col=col.pred)
+
+  # plot(datf$forest_frac~datf$logpop, pch=20, cex=0.1, col=col.obs, xlab = "Pop dens", ylab="Forest Frac")
+  # points(datf$forest_frac~datf$logpop, pch=20, cex=datf$baclass/4, col=col.obs)
+  # plot(datf$forest_frac~datf$logpop, pch=20, cex=0.1, col=col.pred, xlab = "Pop dens", ylab="Forest Frac")
+  # points(datf$forest_frac~datf$logpop, pch=20, cex=datf$baclass_pred/3, col=col.pred)
+  # 
+  dev.off()
+}
+
+setwd(paste0(fire_dir,"/fire_aggregateData/output",suffix,"/figures" ))
+
+plot.niche(datf, "ALL")  # MIXED
+
+
+
+}
 }
 
 png(filename = paste0("niche_scale.png"), width = 512*3, height = 790*3, res = 300)
